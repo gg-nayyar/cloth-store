c81388191d2ec0d49f64c0a2ded8a71c96075aca
gg-nayyar <krishsenpai7@gmail.com>
2025-01-07 21:42:13 +0530

    home page added

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import LoginForm from "../../components/login-form";
+import axios from "axios";
+
+const page = () => {
+    type LoginFormData = {
+        email: string;
+        password: string;
+      };
+      const handleLoginData = async (data: LoginFormData) => {
+        console.log("DATA: ", data);
+        const response = await axios.post<{token:string,user:object}>(
+          "http://localhost:8001/api/login",
+          data,
+          { withCredentials: true }
+        );
+        if(response.data.token){
+          window.location.href = '/home'
+        }
+      }; 
+      return (
+        <div className="flex h-screen w-screen">
+          <div className="h-full w-3/4 bg-skin">
+            <LoginForm onSubmit={handleLoginData} />
+          </div>
+          <div className="h-full bg-skin">
+            <Image
+              className=" p-10 h-full w-full"
+              src="/login.gif"
+              height={100}
+              width={100}
+              alt="Picture of a model"
+              unoptimized
+            />
+          </div>
+        </div>
+      );
+}
+
+export default page
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,39 +1,21 @@
-"use client";
-import React from "react";
+"use client"
+import React from "react"
 import Image from "next/image";
-import LoginForm from "../components/login-form";
-import axios from "axios";
 
 export default function Home() {
-  type LoginFormData = {
-    email: string;
-    password: string;
-  };
-  const handleLoginData = async (data: LoginFormData) => {
-    console.log("DATA: ", data);
-    const response = await axios.post<{token:string,user:object}>(
-      "http://localhost:8001/api/login",
-      data,
-      { withCredentials: true }
-    );
-    if(response.data.token){
-      window.location.href = '/home'
-    }
-  }; 
-  return (
-    <div className="flex h-screen w-screen">
-      <div className="h-full w-3/4 bg-skin">
-        <LoginForm onSubmit={handleLoginData} />
-      </div>
-      <div className="h-full bg-skin">
-        <Image
-          className=" p-10 h-full w-full"
-          src="/login.gif"
-          height={100}
-          width={100}
-          alt="Picture of a model"
-          unoptimized
-        />
+  return(
+    <div className=" flex h-screen w-full">
+      <Image className="bg-cover h-screen w-full -z-50 fixed "
+      src=  "/background.webp"
+      height={100}
+      width={100}
+      alt="Background"
+      unoptimized 
+      />
+      <div className="  flex self-center relative justify-center items-center bg-transparent h-16 w-full">
+      <button className="h-16 w-32">
+        <h1>Explore new catalogue</h1>
+      </button>
       </div>
     </div>
   );
